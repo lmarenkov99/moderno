45ff8709c65fd1584bdf8751990412608f571f11
lmarenkov99 <Lmarenkov99@gmail.com>
2019-10-14 22:23:41 +0300

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,49 +1,83 @@
-$(function () {
+$(function(){
 
 
-    $(".rate-star").rateYo({
-        rating: 5,
-        starWidth: "12px",
-        readOnly: true
-    });
+  $('.rate-star').rateYo({
+    rating: 5,
+    starWidth: "12px",
+    readOnly: true,
+  });
 
-    $('.product-slider-inner').slick({
-        dots: true,
-        arrows: false,
-        slidesToShow: 4,
-        slidesToScroll: 4
-    });
+  $('.product-slider__inner').slick({
+    dots: true,
+    arrows: false,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1900,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        }
+      },
+      {
+        breakpoint: 1441,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 801,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
 
-    $(".js-range-slider").ionRangeSlider({
-        type: "double",
-        min: 0,
-        max: 1000,
-        from: 0,
-        to: 600,
-        prefix: "$"
-    });
+  $(".js-range-slider").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 1000,
+    from: 0,
+    to: 600,
+    prefix: "$"
+});
+  
 
-    $('.icon-th-list').on('click', function () {
-        $('.product__item').addClass('list');
-        $('.icon-th-list').addClass('active');
-        $('.icon-th-large').removeClass('active');
-    });
+$('input[type=file], select').styler();
 
-    $('.icon-th-large').on('click', function () {
-        $('.product__item').removeClass('list');
-        $('.icon-th-large').addClass('active');
-        $('.icon-th-list').removeClass('active');
-    });
+$('.icon-th-list').on('click', function(){
+  $('.product-page__items .product__item').addClass('list');
+  $('.icon-th-list').addClass('active');
+  $('.icon-th-large').removeClass('active');
+});
+$('.icon-th-large').on('click', function(){
+  $('.product-page__items .product__item').removeClass('list');
+  $('.icon-th-large').addClass('active');
+  $('.icon-th-list').removeClass('active');
+})
 
-    $('.menu__btn').on('click', function(){
-        $('.menu__list').slideToggle();
-    });
+$('.product-one__tabs .tab, .settings__tabs .tab').on('click', function(event) {
+  var id = $(this).attr('data-id');
+    $('.product-one__tabs, .settings__tabs').find('.tab-item').removeClass('active-tab').hide();
+    $('.product-one__tabs .tabs, .settings__tabs .tabs').find('.tab').removeClass('active');
+    $(this).addClass('active');
+    $('#'+id).addClass('active-tab').fadeIn();
+    return false;
+  });
 
-    $('.header__btn-menu').on('click', function(){
-        $('.header__box').toggleClass('active');
-    });
+  $('.menu__btn').on('click', function(){
+    $('.menu__list').slideToggle();
+  });
 
-        var mixer = mixitup('.products__inner-box');
-
-
-    });
+  $('.header__btn-menu').on('click', function(){
+    $('.header__box').toggleClass('active');
+  });
+  var mixer = mixitup('.products__inner-box');
+});
